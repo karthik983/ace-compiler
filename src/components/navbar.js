b6ad966ca2aa465dfd5bf204ef4c3e398744d9f2
ip11 <iprash011@gmail.com>
2020-04-16 15:46:53 +0530

    Icon size changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,57 +1,54 @@
-import React from "react";
-import Img from "../img.png";
+import React from 'react';
+import Img from '../img.png';
 function Navbar() {
-  return (
-    <nav
-      className="navbar navbar-expand-sm d-flex justify-content-between  "
-      id="main-nav"
-    >
-      <div className="pl-3">
-        <a href="index.html" className="navbar-brand">
-          <img src={Img} style={{ height: "75%", width: "80%" }} alt="/" />
-        </a>
-      </div>
-      <div>
-        <ul className="navbar-nav  ">
-          <li className="nav-item px-1 ">
-            <a href="#home" className="nav-link">
-              <span style={{ color: "royalblue" }}>
-                <i className="fab fa-twitter fa-2x"></i>
-              </span>
-            </a>
-          </li>
-          <li className="nav-item px-1 ">
-            <a href="#home" className="nav-link">
-              <span style={{ color: "royalblue" }}>
-                <i className="fab fa-github fa-2x"></i>
-              </span>
-            </a>
-          </li>
-          <li className="nav-item px-1">
-            <a href="#explore-head-section" className="nav-link">
-              <span style={{ color: "royalblue" }}>
-                <i className="fab fa-facebook-square fa-2x"></i>
-              </span>
-            </a>
-          </li>
-          <li className="nav-item px-1 ">
-            <a href="#create-head-section" className="nav-link">
-              <span style={{ color: "royalblue" }}>
-                <i className="fab fa-linkedin fa-2x"></i>
-              </span>
-            </a>
-          </li>
-          <li className="nav-item px-1">
-            <a href="#share-head-section" className="nav-link">
-              <span style={{ color: "royalblue" }}>
-                <i className="fab fa-instagram fa-2x"></i>
-              </span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+	return (
+		<nav className="navbar navbar-expand-sm d-flex justify-content-between  " id="main-nav">
+			<div className="pl-3">
+				<a href="index.html" className="navbar-brand">
+					<img src={Img} style={{ height: '50%', width: '50%' }} alt="/" />
+				</a>
+			</div>
+			<div>
+				<ul className="navbar-nav  ">
+					<li className="nav-item  ">
+						<a href="#home" className="nav-link">
+							<span style={{ color: 'royalblue' }}>
+								<i className="fab fa-twitter fa-1x" />
+							</span>
+						</a>
+					</li>
+					<li className="nav-item  ">
+						<a href="#home" className="nav-link">
+							<span style={{ color: 'royalblue' }}>
+								<i className="fab fa-github fa-1x" />
+							</span>
+						</a>
+					</li>
+					<li className="nav-item ">
+						<a href="#explore-head-section" className="nav-link">
+							<span style={{ color: 'royalblue' }}>
+								<i className="fab fa-facebook-square fa-1x" />
+							</span>
+						</a>
+					</li>
+					<li className="nav-item  ">
+						<a href="#create-head-section" className="nav-link">
+							<span style={{ color: 'royalblue' }}>
+								<i className="fab fa-linkedin fa-1x" />
+							</span>
+						</a>
+					</li>
+					<li className="nav-item ">
+						<a href="#share-head-section" className="nav-link">
+							<span style={{ color: 'royalblue' }}>
+								<i className="fab fa-instagram fa-1x" />
+							</span>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
 }
 
 export default Navbar;
