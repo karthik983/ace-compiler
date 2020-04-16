95775054e4777b7eea46e0f6d471be814e78e522
Syed Mansoor Ali <ali.syedmansoor1997@gmail.com>
2020-04-16 14:04:15 +0530

    Ide Templating Completed

--- src/components/editor.js
+++ src/components/editor.js
@@ -5,8 +5,9 @@ import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/mode-python";
 import "ace-builds/src-noconflict/mode-c_cpp";
 import "ace-builds/src-noconflict/mode-java";
-import "ace-builds/src-noconflict/theme-github";
-import "ace-builds/src-noconflict/theme-twilight";
+import "ace-builds/src-noconflict/theme-chaos";
+import "ace-builds/src-noconflict/theme-eclipse";
+import Spin from "../Spin.gif";
 
 // import "ace-builds/src-noconflict/worker-javascript";
 
@@ -14,9 +15,7 @@ function Editor() {
   const [code, setCode] = useState(localStorage.getItem("code") || "");
 
   const [lang, setLang] = useState("javascript");
-  const [mode, setMode] = useState("github");
-  const [height, setHeight] = useState("650px")
-  const [width, setWidth] = useState("1400px")
+  const [mode, setMode] = useState("eclipse");
   const [line, setLine] = useState(1);
   const [char, setChar] = useState(0);
 
@@ -24,37 +23,26 @@ function Editor() {
     setCode(newValue);
   };
   useEffect(() => {
-    localStorage.setItem("code", code)
-  }, [code])
+    localStorage.setItem("code", code);
+  }, [code]);
 
-  const topbar = {
-    display: "flex",
-    justifyContent: "flex-start",
-    alignSelf: "auto",
-  };
-  const container = {
-    marginTop: "30px",
-    marginLeft: "20%",
-    marginRight: "20%",
-  };
   const reset = {
-
     outlineOffset: "-2px",
     border: 0,
-    padding: 0,
+    backgroundColor: "white",
     cursor: "pointer",
-    // eslint no-dupe-keys:1
+
     padding: "0.25rem 0.5rem",
-    margin: "0.25rem"
-  }
+    margin: "0.25rem",
+  };
   const language = (e) => {
     setLang(e.target.value);
   };
   const modeChange = (e) => {
     if (e.target.checked === true) {
-      setMode("twilight");
+      setMode("chaos");
     } else {
-      setMode("github");
+      setMode("eclipse");
     }
   };
   const cursorChange = (selection) => {
@@ -63,71 +51,146 @@ function Editor() {
   };
 
   const clickHandler = () => {
-    setCode("")
-  }
+    setCode("");
+  };
   const screenHandler = () => {
-    setHeight("1500px")
-    setWidth("1500px")
+    // setHeight("1500px");
+    // setWidth("1500px");
     // ace.resize()
-  }
+  };
 
   return (
-    <div id="container" style={container}>
-      <div style={topbar}>
-        <div>Light Mode</div>
+    <div>
+      <div className="container rounded-lg mt-5 px-0 shadow-lg">
+        <div className="d-flex ">
+          <div className="p-2 ml-5 align-self-center ">Light Mode</div>
+          <div className=" pt-3">
+            <label className="switch ">
+              <input
+                className="align-self-center"
+                type="checkbox"
+                onClick={modeChange}
+              />
+              <span className="slider round "></span>
+            </label>
+          </div>
+          <div className="mr-auto p-2 align-self-center">Dark Mode </div>
+
+          <div className=" p-2 reset ">
+            <button
+              style={reset}
+              className="align-self-center"
+              onClick={clickHandler}
+            >
+              <i
+                className="fa fa-refresh fa-2x"
+                aria-hidden="true"
+                style={{
+                  width: "35px",
+                  height: "35px",
+                  color: "darkgray",
+                  paddingTop: "2px",
+                }}
+              ></i>
+            </button>
+          </div>
+          <div className="p-2 fullscreen">
+            <button style={reset} onClick={screenHandler}>
+              <i
+                className="fa fa-expand fa-2x "
+                aria-hidden="true"
+                style={{
+                  width: "35px",
+                  height: "35px",
+                  color: "darkgray",
+                  paddingTop: "2px",
+                }}
+              ></i>
+            </button>
+          </div>
+          <div className="p-2 mr-5 align-self-center">
+            <select
+              value={lang}
+              className="form-control mx-1 "
+              onChange={language}
+            >
+              <option value="javascript">Javascript</option>
+              <option value="python">Python</option>
+              <option value="c_cpp">C / C++</option>
+              <option value="java">Java</option>
+            </select>
+          </div>
+        </div>
         <div>
-          <label className="switch mx-2">
-            <input type="checkbox" onClick={modeChange} />
-            <span className="slider round"></span>
-          </label>
+          <AceEditor
+            mode={lang}
+            value={code}
+            theme={mode}
+            onChange={onChange}
+            showPrintMargin={false}
+            name="UNIQUE_ID_OF_DIV"
+            wrapEnabled={true}
+            width="1140px"
+            height="700px"
+            fontSize={25}
+            onCursorChange={cursorChange}
+            editorProps={{ $blockScrolling: true }}
+          />
         </div>
-        <div>Dark Mode </div>
-        <div className="reset" >
-          <button style={reset} onClick={clickHandler}><i className="fa fa-refresh" aria-hidden="true" style={{ width: "25px", height: "25px" }}></i></button>
+        <div className="d-flex">
+          <div className="p-2 fullscreen">
+            <button style={reset} onClick={screenHandler}>
+              <i
+                className="fa fa-download fa-2x "
+                aria-hidden="true"
+                style={{ width: "35px", height: "35px", color: "darkgray" }}
+              ></i>
+            </button>
+          </div>
+          <div></div>
+          <div className="ml-auto align-self-center mx-4 ">
+            <div className="py-3 mr-4" style={{ fontSize: "20px" }}>
+              Line : {line} Column : {char}
+            </div>
+          </div>
         </div>
-
-        <div className="fullscreen" >
-          <button style={reset} onClick={screenHandler} ><i className="fa fa-compress" aria-hidden="true" style={{ width: "25px", height: "25px" }}></i></button>
-        </div>
-
-        <div>
-          <select
-            value={lang}
-            className="form-control mx-5"
-            onChange={language}
-          >
-            <option value="javascript">Javascript</option>
-            <option value="python">Python</option>
-            <option value="c_cpp">C / C++</option>
-            <option value="java">Java</option>
-          </select>
-        </div>
-
       </div>
-      <div  >
-        <AceEditor
-          mode={lang}
-          value={code}
-          theme={mode}
-          onChange={onChange}
-          showPrintMargin={false}
-          name="UNIQUE_ID_OF_DIV"
-          wrapEnabled={true}
-          enableBasicAutocompletion={true}
-          height={height}
-          width={width}
-          fontSize={25}
-          onCursorChange={cursorChange}
-          editorProps={{ $blockScrolling: true }}
-        />
-      </div>
-      <div style={topbar}>
-        <p>
-          Line : {line} , {char}
-        </p>
-        <button type="button" class="btn btn-secondary">
-          Submit
+      <div className="container px-0 my-5 d-flex">
+        <button type="button" className="btn btn-primary btn-lg px-5  ml-auto ">
+          Compile
         </button>
+      </div>
+      <div className="shadow-lg container rounded-lg px-0 my-5 ">
+        <div>
+          <div className="d-flex justify-content-between p-1">
+            <div className="align-self-center ml-3">
+              Status : Successfully executed{" "}
+            </div>
+            <div className="align-self-center">
+              {" "}
+              Date : 2020-04-16 13:00:00{" "}
+            </div>
+            <div className="align-self-center">Time : 0 Sec </div>
+            <div className="align-self-center">Memory : 9.128 kB</div>
+            <div className="fullscreen">
+              <button style={reset} onClick={screenHandler}>
+                <i
+                  className="fa fa-times fa-2x "
+                  aria-hidden="true"
+                  style={{ width: "35px", height: "35px", color: "darkgray" }}
+                ></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="m-auto">
+          <textarea
+            className="form-control "
+            rows="5"
+            style={{ resize: "none", backgroundColor: "beige" }}
+            // Misty Rose ""mistyrose || Beige "beige"
+          ></textarea>
+        </div>
       </div>
     </div>
   );
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,55 @@
 import React from "react";
-
+import Img from "../img.png";
 function Navbar() {
   return (
-    <nav>
-      <span>
-        <img
-          src="https://code.in/images/logo-gif.gif"
-          alt="code.inlogo"
-          height="50px"
-        />
-      </span>
+    <nav
+      className="navbar navbar-expand-sm d-flex justify-content-between  "
+      id="main-nav"
+    >
+      <div className="pl-3">
+        <a href="index.html" className="navbar-brand">
+          <img src={Img} style={{ height: "75%", width: "75%" }} alt="/" />
+        </a>
+      </div>
+      <div>
+        <ul className="navbar-nav  ">
+          <li className="nav-item px-1 ">
+            <a href="#home" className="nav-link">
+              <span style={{ color: "royalblue" }}>
+                <i className="fab fa-twitter fa-2x"></i>
+              </span>
+            </a>
+          </li>
+          <li className="nav-item px-1 ">
+            <a href="#home" className="nav-link">
+              <span style={{ color: "royalblue" }}>
+                <i className="fab fa-github fa-2x"></i>
+              </span>
+            </a>
+          </li>
+          <li className="nav-item px-1">
+            <a href="#explore-head-section" className="nav-link">
+              <span style={{ color: "royalblue" }}>
+                <i className="fab fa-facebook-square fa-2x"></i>
+              </span>
+            </a>
+          </li>
+          <li className="nav-item px-1 ">
+            <a href="#create-head-section" className="nav-link">
+              <span style={{ color: "royalblue" }}>
+                <i className="fab fa-linkedin fa-2x"></i>
+              </span>
+            </a>
+          </li>
+          <li className="nav-item px-1">
+            <a href="#share-head-section" className="nav-link">
+              <span style={{ color: "royalblue" }}>
+                <i className="fab fa-instagram fa-2x"></i>
+              </span>
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
