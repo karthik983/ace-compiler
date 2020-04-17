47f9484d1edffa216ff1df726fa73c9b682a10ff
karthik983 <karthikh032@gmail.com>
2020-04-17 17:05:14 +0530

    final v1 ui

--- src/components/editor.js
+++ src/components/editor.js
@@ -14,7 +14,8 @@ function Editor() {
 	const [ code, setCode ] = useState(localStorage.getItem('code') || '');
 	const [ js, setJs ] = useState(localStorage.getItem('js') || ' ');
 	const [ python, setPython ] = useState(localStorage.getItem('python') || ' ');
-	const [ c, setC ] = useState(localStorage.getItem('c') || ' ');
+  const [ c, setC ] = useState(localStorage.getItem('c') || ' ');
+  const [pyth2,setPyth2]=useState(localStorage.getItem("pyth2")||" ")
 	const [ font, setFont ] = useState(18);
 	const [ lang, setLang ] = useState('javascript');
 	const [ mode, setMode ] = useState('katzenmilch');
@@ -31,21 +32,25 @@ function Editor() {
 			setJs(newValue);
 		} else if (lang === 'c_cpp') {
 			setC(newValue);
-		}
+    }
+    else if(lang==="python2.7"){
+      setPyth2(newValue)
+    }
 	};
 	useEffect(
 		() => {
 			localStorage.setItem('code', code);
 			localStorage.setItem('python', python);
 			localStorage.setItem('js', js);
-			localStorage.setItem('c', c);
+      localStorage.setItem('c', c);
+      localStorage.setItem("pyth2",pyth2)
 			const fetch = async () => {
 				const iPadd = await axios.get('https://api.ipify.org');
 				setIp(iPadd.data);
 			};
 			fetch();
 		},
-		[ code, python, js, c ]
+		[ code, python, js, c, pyth2 ]
 	);
 
 	const reset = {
@@ -59,7 +64,8 @@ function Editor() {
 	};
 
 	const language = (e) => {
-		setLang(e.target.value);
+      setLang(e.target.value);
+		
 		if (e.target.value === 'javascript') {
 			if (code !== js) {
 				setCode(js);
@@ -72,11 +78,18 @@ function Editor() {
 			} else {
 				setCode('//type your C code here');
 			}
-		} else {
+		} else if(e.target.value === 'python'){
 			if (code !== python) {
 				setCode(python);
 			} else {
 				setCode('#type your python code here');
+			}
+    }
+    else if(e.target.value === 'python2.7'){
+			if (code !== pyth2) {
+				setCode(pyth2);
+			} else {
+				setCode('#type your python 2.7 code here');
 			}
 		}
 	};
@@ -100,10 +113,13 @@ function Editor() {
 		} else if (lang === 'c_cpp') {
 			setCode('//type your C code here');
 			setC('//type your C code here');
-		} else {
+		} else if(lang==="python") {
 			setCode('#type your python code here');
 			setPython('#type your python code here');
-		}
+		}else if(lang==="python2.7"){
+      setCode('#type your python 2.7 code here')
+      setPyth2('#type your python 2.7 code here')
+    }
 	};
 
 	const setLoad = () => {
@@ -231,13 +247,14 @@ function Editor() {
 						<select value={lang} className="form-control mx-1 " onChange={language}>
 							<option value="javascript">Javascript(Node.js)</option>
 							<option value="python">Python 3</option>
-							<option value="c_cpp">C</option>
+							<option value="c_cpp">C Language</option>
+              <option value="python2.7">Python 2.7 </option>
 						</select>
 					</div>
 				</div>
 				<div>
 					<CodeEditor
-						mode={lang}
+						mode={lang==="python2.7"?"python":lang}
 						value={code}
 						theme={mode}
 						onChange={onChange}
